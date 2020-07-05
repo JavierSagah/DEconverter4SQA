5159cd509f88e1f919d7c7f768eca4d5b9089af6
JavierSagah <saldarr2@uwm.edu>
2020-07-05 16:02:42 -0500

    de2py method added, it allows for traversin a directory and converting every csv file in it

--- generate_csv.py
+++ generate_csv.py
@@ -94,6 +94,52 @@ def convert_test_file2(input_file, output_file,  mode):
 
 
 # generate_test_file(suite_test_id)
-convert_test_file1('tests/test_input/j_install_DE1.csv', 'tests/output_testfiles/translated_bde.csv')
+# convert_test_file1('tests/test_input/j_install_DE1.csv', 'tests/output_testfiles/translated_bde.csv')
 # convert_test_file2('tests/test_input/j_install_DE1.csv', 'tests/output_testfiles/translated_bde1.csv', 'w')
 # convert_test_file2('tests/test_input/test.csv', 'tests/output_testfiles/translated_bde2.csv', 'a')
+
+
+# #### Traversing a directory for its contents using absolute or relative paths ####
+# # 1st method, import the os module, for the os.walk function
+# import os
+#
+# # Set the directory you want to start from
+# rootDir_relative = './tests/test_input'
+# rootDir_absolute = '/Users/Javier/Documents/BadgerMeter/projects/DEconverter4SQA/tests/test_input'
+#
+# for dirName, subdirList, fileList in os.walk(rootDir_relative):    # os.walk(path) returns a triple (dirname,
+#                                                                     # subdirList, fileList) Now, subdirList is a list of
+#                                                                     # all dirs inside that path and fileList is a list
+#                                                                     # of all files inside that path including the ones
+#                                                                     # inside dirs inside that path
+#     print('Found file: %s' % fileList)
+# #     for fname in fileList:
+# #         print('\t%s' % fname)
+# print("--------")
+# for dirName, subdirList, fileList in os.walk(rootDir_absolute):    # os.walk(path) returns a triple (dirname, subdirList, fileList)
+#     print('Found file: %s' % fileList)
+#
+# # 2nd method, using glob
+# import glob
+#
+# # Print png images in folder C:\Users\admin\
+# for filepath in glob.iglob(r'C:\Users\admin\*.png'):
+#     print(filepath)
+#
+# # Print pdf files in folder C:\Users\admin\
+# for filepath in glob.iglob(r'C:\Users\admin\*.pdf'):
+#     print(filepath)
+# #### End of traversing dirs ####
+
+
+def de2py(input_dir, output_dir):
+    import os
+    dir_path = './' + input_dir
+    mode = 'w'
+    for filename in os.listdir(dir_path): # os.listdir(path) returns a list of all contents in path (files and dirs)
+        if '.csv' in filename:
+            convert_test_file2(input_dir + '/' + filename, output_dir + '/d2p_' + filename, mode)
+            mode = 'a'
+
+
+de2py('tests/test_input', 'tests/output_testfiles')
